095d572c71183422fe6ba3748b3f03607f8261da
scx42 <sunchan@sheridancollege.ca>
2020-01-23 14:43:58 -0500

    First Commit of the repo

---
+++ src/gitreview/GitReview.java
@@ -0,0 +1,17 @@
+package gitreview;
+
+/**
+ * This class +++Insert Description Here+++
+ * 
+ * @author Changxin Sun
+ */
+public class GitReview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+    }
+
+}
